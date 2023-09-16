3add160a35d06ed54eb6801cb419fcc6e0728cca
aliemre2023 <aliemrekaya239@gmail.com>
2023-09-17 00:06:10 +0300

    169 24 27

---
+++ medium/0024_swap-nodes-in-pairs.cpp
@@ -0,0 +1,30 @@
+// Time Comlexity = O(n)
+// Space Complexity = O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* curr = head;
+
+        while(curr != nullptr && curr->next != nullptr){
+            int temp1 = curr->val;
+            curr->val = curr->next->val;
+            curr = curr->next;
+            curr->val = temp1;
+
+            curr = curr->next;
+
+        }
+        return head;
+    }
+};
