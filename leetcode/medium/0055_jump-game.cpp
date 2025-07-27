99ed03de5c4fe8f3b9b71f53974500ad9ad32948
aliemre2023 <aliemrekaya239@gmail.com>
2025-07-27 15:44:55 +0300

    revise for 55

--- leetcode/medium/0055_jump-game.cpp
+++ leetcode/medium/0055_jump-game.cpp
@@ -2,30 +2,17 @@
 // Space Complexity = O(1)
 // Question Link = https://leetcode.com/problems/jump-game/
 
+// this solution is shorter than old one,  
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i = 0;
-        while (i < nums.size()) {
-            int best_jump = 0;
-            int best_jump_idx = i;
-            if ((best_jump_idx + nums[best_jump_idx]) >= nums.size() - 1) {
-                return true;
-            }
+        int n = nums.size();
+        int maxi = 0;
 
-            for (int j = i + 1; j <= i + nums[i]; j++) {
-                if (j + nums[j] > best_jump) {
-                    best_jump = j + nums[j];
-                    best_jump_idx = j;
-                }
-            }
-
-            if (best_jump_idx == i) {
-                return false;
-            }
-            
-            i = best_jump_idx;
+        for(int i = 0; i < nums.size() && i <= maxi; i++){
+            maxi = max(maxi, i+nums[i]);    
         }
-        return false;
+
+        return maxi >= n-1;
     }
 };
